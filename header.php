7b3a8471a9295dc191f1206c4cbe0bf28baa9867
Sepehr Sheikh <sepishkh@gmail.com>
2026-06-07 10:39:14 +0330

    Link header title to homepage

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
-<h1> Absolute Cinema </h1>
+<a href="index.php">
+    <h1 class="header-title"> Absolute Cinema </h1>
+</a>
 <nav>
     <a href="index.php">Home</a> | <a href="posts.php">All Posts</a> | <a href="categories">Categories</a>
 </nav>
